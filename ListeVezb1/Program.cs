392244cf995a625bea6b1da5f895246cb7030acb
Marko Djokic <marko1319@its.edu.rs>
2020-12-29 22:11:29 +0100

    Add project files.

---
+++ ListeVezb1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListeVezb1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> godine = new List<int>() { 2009, 2010, 2011, 2012, 2013, 2014 };
+
+            Console.Write("Pre dodavanja elemenata.");
+            Console.WriteLine();
+            Console.Write("Broj elemenata: " + godine.Count);
+            Console.WriteLine();
+            Console.Write("Kapacitet: " + godine.Capacity);
+            Console.WriteLine();
+
+            Console.WriteLine();
+
+            godine.Add(2015);
+            godine.Add(2016);
+            godine.Add(2017);
+
+            Console.Write("Posle dodavanja elemenata.");
+            Console.WriteLine();
+            Console.Write("Broj elemenata: " + godine.Count);
+            Console.WriteLine();
+            Console.Write("Kapacitet: " + godine.Capacity);
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
